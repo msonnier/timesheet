c5660fa96b94dce4ceb7f3bd08ec872f84ac3ad3
msonnier <mike@sonnier.name>
2019-07-25 15:05:01 -0500

    Update constants.rb
    Personalize to 2992

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,13 +1,13 @@
 module Constants
-  TIMEZONE = 'Eastern Time (US & Canada)'
+  TIMEZONE = 'Central Time (US & Canada)'
   DAY_END = 1
   BUILD_MEETINGS = 2
   PRE_MEETINGS = 1
   PRE_HOURS = 25
-  DEFAULT_LOGIN = "signup@team900.org"
-  TEAM_NAME = 'Zebracorns'
-  TEAM_NUMBER = '900'
-  SCHOOL_NAME_REPLACEMENT = 'School'
+  DEFAULT_LOGIN = "mandeville.robotics@gmail.com"
+  TEAM_NAME = 'SS Prometheus  '
+  TEAM_NUMBER = '2992 '
+  SCHOOL_NAME_REPLACEMENT = 'MHS'
   GENDERS = ['Male', 'Female', 'Other']
   FRESHMAN_HOURS = 6
   SOPHOMORE_HOURS = 8
